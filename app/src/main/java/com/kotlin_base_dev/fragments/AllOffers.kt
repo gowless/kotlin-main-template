cb7f0bb5f3e9d6cba74950333c0d89b3f551a0d4
Denys <mr.allspit@gmail.com>
2020-12-30 17:31:31 +0200

    added test layouts added drawables and fonts

--- app/src/main/java/com/kotlin_base_dev/fragments/AllOffers.kt
+++ app/src/main/java/com/kotlin_base_dev/fragments/AllOffers.kt
@@ -7,9 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.kotlin_base_dev.R
 
-
-
-
 class AllOffers : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
